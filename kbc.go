11909c2a6e70bbf47fc41579dec4613f20f937cd
Peter Nuttall <psn@nuttall.im>
2018-01-30 15:25:29 +0000

    checkpoint

---
+++ kbc.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+)
+
+func main() {
+	f := "/Users/psn/Downloads/wat/Current Account Statement - 01 Oct 2017.txt"
+	fd, err := s.Open(f)
+	defer fd.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(fd)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmr.Println("vim-go")
+}
