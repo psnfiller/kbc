84c34dc695fd57d21cb5b427866023fa27a6bfe0
Peter Nuttall <psn@nuttall.im>
2018-01-30 15:35:50 +0000

    checkpoint

--- kbc.go
+++ kbc.go
@@ -6,10 +6,11 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
-	re := regexp.MustCompile(`\s+(\d\d [A-Z][a-z]{2} 201\d)\s+(.*)\s\s`)
+	re := regexp.MustCompile(`\s+(\d\d [A-Z][a-z]{2} 201\d)\s+(.*)\s\s+(\d+\.\d+)\s\s+([,0-9]+\.\d+)`)
 	f := "/Users/psn/Downloads/wat/Current Account Statement - 01 Oct 2017.txt"
 	fd, err := os.Open(f)
 	defer fd.Close()
@@ -23,7 +24,7 @@ func main() {
 		if l == nil {
 			continue
 		}
-		fmt.Printf("%s %s\n", l[1], l[2])
+		fmt.Printf("%s %s %s %s\n", l[1], strings.TrimSpace(l[2]), l[3], l[4])
 
 	}
 	if err := scanner.Err(); err != nil {
